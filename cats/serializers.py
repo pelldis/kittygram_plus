4a43f13cbdd9fe6544e8bb2ad5528b93ae02dc80
Anton Kozlov <anton-kozlov@yandex-team.ru>
2023-01-14 06:51:29 +0300

    + token authentication

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,89 @@
+import datetime as dt
+import webcolors
 from rest_framework import serializers
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
 
-from .models import Cat
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
+
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # owner = serializers.StringRelatedField(read_only=True)
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
+    #color = Hex2NameColor()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for archievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **archievement
+            )
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat
+            )
+        return cat
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.color = validated_data.get('color', instance.color)
+        instance.birth_year = validated_data.get('birth_year', instance.birth_year)
+        instance.owner = validated_data.get('owner', instance.owner)
+        instance.save()
+        if 'achievements' in self.initial_data:
+            achievements = validated_data.pop('achievements')
+            cat = Cat.objects.get(id=instance.id)
+            for archievement in achievements:
+                current_achievement, status = Achievement.objects.get_or_create(
+                    **archievement
+                )
+                AchievementCat.objects.create(
+                    achievement=current_achievement, cat=cat
+                )
+        return instance
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,16 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-
-from cats.views import CatViewSet
+from rest_framework.authtoken import views
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owner', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
