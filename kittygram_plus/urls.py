ad06396dfdb21efd29b9fc8672810d35d063b371
Anton Kozlov <anton-kozlov@yandex-team.ru>
2023-01-14 09:05:02 +0300

    JWT authentication

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -12,5 +12,7 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    #path('api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
